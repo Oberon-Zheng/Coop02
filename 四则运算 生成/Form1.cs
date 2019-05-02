4383c0576d078bc9a49310a0e3ffd88e47f56b21
Oberon <360119124@qq.com>
2019-05-02 20:11:49 +0800

    ExprBuilder rewritten. List inplemented.

--- 四则运算 生成/Form1.cs
+++ 四则运算 生成/Form1.cs
@@ -12,42 +12,43 @@ namespace 四则运算_生成
 {
     public partial class FrmQuizGen : Form
     {
-        private ExprBuilder eBuilder;
-        private List<Expr> questions;//存储生成题目列表
-        private class QuizNumber
+        //private ExprBuilder eBuilder;
+        private List<Expr> questions = new List<Expr>();//存储生成题目列表
+        private class QuizConfig
         {
-            private int qcount;
-            public bool CplxFlag;
+            public ExprBuilder eBuilder;
+            public int qcount;
+            //public bool CplxFlag;
             public int addcount, subcount, mulcount, divcount;
             public int Count
             {
                 get
                 {
-                    if (CplxFlag)
+                    if (eBuilder.allowCplxExpr)
                         return qcount;
                     else
                         return addcount + subcount + mulcount + divcount;
                 }
             }
-            public QuizNumber(int addcount, int subcount, int mulcount, int divcount)
+            public QuizConfig(int addcount, int subcount, int mulcount, int divcount)
             {
+                eBuilder = new ExprBuilder();
                 this.addcount = addcount;
                 this.subcount = subcount;
                 this.mulcount = mulcount;
                 this.divcount = divcount;
             }
         }
-        QuizNumber qnum;
+        QuizConfig qConfig;
         public FrmQuizGen()
         {
-            eBuilder = new ExprBuilder();
-            qnum = new QuizNumber(0,0,0,0);
+            qConfig = new QuizConfig(0, 0, 0, 0);
             InitializeComponent();
         }
 
         private void stpQSub_ValueChanged(object sender, EventArgs e)
         {
-            qnum.subcount = decimal.ToInt32((sender as NumericUpDown).Value);
+            qConfig.subcount = decimal.ToInt32((sender as NumericUpDown).Value);
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
@@ -65,6 +66,10 @@ namespace 四则运算_生成
             UpdateCplxSettings();
             UpdateStepper();
             stpMaxPrecision.Enabled = cBoxAllowFrac.Checked;
+            BindingSource bs =  new BindingSource();
+            bs.DataSource = questions;
+            lBoxQuizDisp.DataSource = bs;
+            lBoxQuizDisp.DisplayMember = "";
         }
 
         private void cBoxQTypeAdd_CheckedChanged(object sender, EventArgs e)
@@ -89,13 +94,14 @@ namespace 四则运算_生成
 
         private void stpMaxOptr_ValueChanged(object sender, EventArgs e)
         {
-            eBuilder.maxOpnd = decimal.ToInt32(stpMaxOptr.Value);
+            qConfig.eBuilder.maxOpnd = decimal.ToInt32(stpMaxOptr.Value);
         }
 
         private void cBoxAllowCplx_CheckedChanged(object sender, EventArgs e)
         {
             UpdateCplxSettings();
             UpdateStepper();
+            qConfig.eBuilder.allowCplxExpr = (sender as CheckBox).Checked;
         }
 
         private void UpdateCplxSettings()
@@ -131,18 +137,18 @@ namespace 四则运算_生成
 
         private void stpCQuizNum_ValueChanged(object sender, EventArgs e)
         {
-
+            qConfig.qcount = decimal.ToInt32((sender as NumericUpDown).Value);
         }
 
         private void cBoxCQuizAdd_CheckedChanged(object sender, EventArgs e)
         {
-            if((sender as CheckBox).Checked)
+            if ((sender as CheckBox).Checked)
             {
-                eBuilder.Allow(ExprOprt.ADD);
+                qConfig.eBuilder.Allow(ExprOprt.ADD);
             }
             else
             {
-                eBuilder.Disallow(ExprOprt.ADD);
+                qConfig.eBuilder.Disallow(ExprOprt.ADD);
             }
         }
 
@@ -150,11 +156,11 @@ namespace 四则运算_生成
         {
             if ((sender as CheckBox).Checked)
             {
-                eBuilder.Allow(ExprOprt.SUB);
+                qConfig.eBuilder.Allow(ExprOprt.SUB);
             }
             else
             {
-                eBuilder.Disallow(ExprOprt.SUB);
+                qConfig.eBuilder.Disallow(ExprOprt.SUB);
             }
         }
 
@@ -162,11 +168,11 @@ namespace 四则运算_生成
         {
             if ((sender as CheckBox).Checked)
             {
-                eBuilder.Allow(ExprOprt.MUL);
+                qConfig.eBuilder.Allow(ExprOprt.MUL);
             }
             else
             {
-                eBuilder.Disallow(ExprOprt.MUL);
+                qConfig.eBuilder.Disallow(ExprOprt.MUL);
             }
         }
 
@@ -174,37 +180,37 @@ namespace 四则运算_生成
         {
             if ((sender as CheckBox).Checked)
             {
-                eBuilder.Allow(ExprOprt.DIV);
+                qConfig.eBuilder.Allow(ExprOprt.DIV);
             }
             else
             {
-                eBuilder.Disallow(ExprOprt.DIV);
+                qConfig.eBuilder.Disallow(ExprOprt.DIV);
             }
         }
 
         private void cBoxCQuizAllowBrack_CheckedChanged(object sender, EventArgs e)
         {
-            eBuilder.allowBrack = (sender as CheckBox).Checked;
+            qConfig.eBuilder.allowBrack = (sender as CheckBox).Checked;
         }
 
         private void stpMaxPrecision_ValueChanged(object sender, EventArgs e)
         {
-            eBuilder.maxPrecision = decimal.ToInt32((sender as NumericUpDown).Value);
+            qConfig.eBuilder.maxPrecision = decimal.ToInt32((sender as NumericUpDown).Value);
         }
 
         private void stpQAdd_ValueChanged(object sender, EventArgs e)
         {
-            qnum.addcount = decimal.ToInt32((sender as NumericUpDown).Value);
+            qConfig.addcount = decimal.ToInt32((sender as NumericUpDown).Value);
         }
 
         private void stpQMul_ValueChanged(object sender, EventArgs e)
         {
-            qnum.mulcount = decimal.ToInt32((sender as NumericUpDown).Value);
+            qConfig.mulcount = decimal.ToInt32((sender as NumericUpDown).Value);
         }
 
         private void stpQDiv_ValueChanged(object sender, EventArgs e)
         {
-            qnum.divcount = decimal.ToInt32((sender as NumericUpDown).Value);
+            qConfig.divcount = decimal.ToInt32((sender as NumericUpDown).Value);
         }
 
         private void lBoxQuizDisp_SelectedIndexChanged(object sender, EventArgs e)
@@ -214,38 +220,62 @@ namespace 四则运算_生成
 
         private void btnGenerate_Click(object sender, EventArgs e)
         {
+            Random r = new Random();
+            try
             {
-                for(var i=0;i<qnum.Count;i++)
+                GenerateQuestions(r);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("未选定合适的运算符！");
+            }
+            return;
+        }
+
+        private void GenerateQuestions(Random r)
+        {
+            if(qConfig.eBuilder.allowOprt == (byte)ExprOprt.NIL)
+            {
+                throw new NoOprtFoundException();
+            }
+            for (var i = 0; i < qConfig.Count; i++)
+            {
+                try
                 {
-                    try
-                    {
-                        Expr gen = eBuilder.Generate();
-                        if(gen==null)
-                        {
-                            i--;
-                            continue;
-                        }
-                        questions.Add(gen);
-                        if (eBuilder.allowCplxExpr)
-                        {
-                            /*switch (gen.oprt)
-                            {
-                                case ExprOprt.ADD:
-                                    qnum.addcount++;
-                                    break;
-                                default:
-                            }
-                            {
-                                
-                            }*/
-                        }
-                    }
-                    catch (Exception)
+                    Expr egen = qConfig.eBuilder.Generate(r);
+                    if (egen == null)
                     {
                         i--;
                         continue;
                     }
+                    questions.Add(egen);
+                    if (qConfig.eBuilder.allowCplxExpr)
+                    {
+                        switch (egen.oprt)
+                        {
+                            case ExprOprt.ADD:
+                                qConfig.addcount++;
+                                break;
+                            case ExprOprt.SUB:
+                                qConfig.subcount++;
+                                break;
+                            case ExprOprt.MUL:
+                                qConfig.mulcount++;
+                                break;
+                            case ExprOprt.DIV:
+                                qConfig.divcount++;
+                                break;
+                            default:
+                                throw new InvalidExprOprtParseException(egen.oprt);
+                        }
+                    }
                 }
+                catch (Exception)
+                {
+                    i--;
+                    continue;
+                }
+                //questions.Add(egen);
             }
         }
     }
